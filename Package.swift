5714fc7ef5b1882e4415da3976fa1fa37b85eddf
Dustin Mote <dustin@vivrelle.com>
2025-11-07 14:32:58 -0800

    Point to fork of opentelemetry-swift-core the has OpenTelemeryAPI through Datadog's implementation

--- Package.swift
+++ Package.swift
@@ -5,26 +5,9 @@ import CompilerPluginSupport
 import Foundation
 import PackageDescription
 
-#if TUIST
-    import ProjectDescription
 
-    let packageSettings = PackageSettings(
-        productTypes: [
-            "KSCrash": .framework,
-            "OpenTelemetrySdk": .framework,
-            "OpenTelemetryApi": .framework
-        ]
-    )
-#endif
+var linkerSettings: [LinkerSetting]? = []
 
-var linkerSettings: [LinkerSetting]?
-
-// This applies only to targets like EmbraceCore and EmbraceIO that contain `@objc extensions`.
-// When linked statically (as Tuist tends to do when installing Embrace via SPM packages),
-// selectors from these extensions are stripped unless `-ObjC` is passed explicitly to the linker.
-if ProcessInfo.processInfo.environment["EMBRACE_ENABLE_TUIST_OBJC_LINK"] != nil {
-    linkerSettings = [.unsafeFlags(["-ObjC"])]
-}
 
 let package = Package(
     name: "EmbraceIO",
@@ -46,8 +29,8 @@ let package = Package(
             from: "2.4.0"
         ),
         .package(
-            url: "https://github.com/open-telemetry/opentelemetry-swift-core",
-            from: "2.1.1"
+            url: "https://github.com/Dmote-V/opentelemetry-swift-core",
+            branch: "datadog-package"
         ),
         .package(
             url: "https://github.com/swiftlang/swift-syntax.git",
